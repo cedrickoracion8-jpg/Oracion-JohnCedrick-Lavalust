42fb62cc10cb8fa7413e1d6f924f25f3dba88bcd
cedrickoracion8 <cedrickoracion8@gmail.com>
2026-08-21 23:17:30 +0800

    hehe

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -42,3 +42,6 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 $config['middlewares'] = [];
+$routeMiddleware = [
+    'student' => StudentMiddleware::class,
+];
